015b480f2e70a1a1d6974d6fdc0a2523c0b46a2f
Connor Claypool <ctclaypool@protonmail.ch>
2018-07-30 18:54:12 +0100

    Complete draft/beta code

--- App1/server.R
+++ App1/server.R
@@ -1,13 +1,12 @@
 library(shiny)
 library(ggplot2)
-library(plotly)
 library(Hmisc)
 data(diamonds)
 
 shinyServer(function(input, output) {
 
     output$data_head <- renderTable({
-        ord <- order(as.data.frame(diamonds)[input$variable])
+        ord <- order(as.data.frame(diamonds)[[input$variable]])
         if(input$asc == FALSE) {
             ord <- rev(ord)
         }
@@ -26,42 +25,159 @@ shinyServer(function(input, output) {
         paste("First",  as.character(input$obs),  "of", as.character(nrow(diamonds)), "observations")
     })
     
-    output$plot_var <- renderPlot({
-        var <- data.frame(x = diamonds[[input$variable]])
-        g <- ggplot(var, aes(x = x)) + 
-            xlab("") +
-            ylab("")
-        if(class(var$x)[1] == "numeric") {
-            g + geom_histogram(fill = "steelblue", bins = 15)
+    output$plot_scatter <- renderPlot({
+        data <- data.frame(x = diamonds[[input$x_scatter]], y = diamonds[[input$y_scatter]])
+        if(input$color_scatter != "None") {
+            data$color <- diamonds[[input$color_scatter]]
+            aes <- aes(x = x, y = y, color = color)
         } else {
-            g + geom_bar(fill = "darkred") + 
-                theme(axis.text.x = element_text(angle = 90, hjust = 1))
+            aes <- aes(x = x, y = y)
         }
+        if(input$facet_scatter != "None") {
+            data$facet <- diamonds[[input$facet_scatter]]
+            facet <- facet_grid(. ~ facet)
+        } else {
+            facet <- NULL
+        }
+        if(input$smooth_scatter == "Regression Line") {
+            smooth <- geom_smooth(method = "lm")
+        } else if(input$smooth_scatter == "Smooth Curve") {
+            smooth <- geom_smooth(method = "auto")
+        } else {
+            smooth <- NULL
+        }
+        ggplot(data, aes) + 
+            smooth +
+            geom_point(size = input$size_scatter, alpha = input$alpha_scatter) +
+            xlab(input$x_scatter) +
+            ylab(input$y_scatter) +
+            labs(color = input$color_scatter) +
+            facet
     })
     
-    output$plot_var_title <- renderText({
-        paste("Distribution of Variable:", input$variable)
+    output$plot_box <- renderPlot({
+        data <- data.frame(y = diamonds[[input$y_box]])
+        if(class(diamonds[[input$x_box]])[1] == "numeric") {
+            data$x <- cut2(diamonds[[input$x_box]], g = 6)
+        } else {
+            data$x <- diamonds[[input$x_box]]
+        }
+        if(input$dimension3_box == "Color") {
+            data$color <- diamonds[[input$color_box]]
+            aes <- aes(x = x, y = y, color = color)
+        } else {
+            aes <- aes(x = x, y = y)
+        }
+        x_axis_labels_flip <- NULL
+        if(input$dimension3_box == "Faceting") {
+            data$facet <- diamonds[[input$facet_box]]
+            if(!input$rotate_box) {
+                x_axis_labels_flip <- theme(axis.text.x = element_text(angle = 90, hjust = 1))
+                facet <- facet_grid(. ~ facet)
+            } else {
+                facet <- facet_grid(facet ~ .)
+            }
+        } else {
+            facet <- NULL
+        }
+        if(input$rotate_box) {
+            coord_flip <- coord_flip()
+        } else {
+            coord_flip <- NULL
+        }
+        ggplot(data, aes) +
+            geom_boxplot() +
+            xlab(input$x_box) +
+            ylab(input$y_box) +
+            labs(color = input$color_box) +
+            facet +
+            coord_flip +
+            x_axis_labels_flip
     })
     
-    output$plot1 <- renderPlot({
-        data <- data.frame(x = diamonds[[input$x]], y = diamonds[[input$y]], colour = FALSE)
-        if(input$type == "Box Plot" & class(data$x) == "numeric") {
-            data$x <- cut2(data$x, g = 6)
-        }
-        if(input$colour == "None") {
-            g <- ggplot(data, aes(x, y))
+    output$plot_freq <- renderPlot({
+        geoms <- c("geom_histogram", "geom_density", "geom_bar")
+        names(geoms) <- c("Histogram", "Density Plot", "Bar Plot")
+        if(input$freq_subtype != "Bar Plot") {
+            data <- data.frame(x = diamonds[[input$freq_x_numeric]])
+            xlab <- input$freq_x_numeric
         } else {
-            data$z <- diamonds[[input$colour]]
-            g <- ggplot(data, aes(x, y, color = z))
+            data <- data.frame(x = diamonds[[input$freq_x_discrete]])
+            xlab <- input$freq_x_discrete
         }
-        g <- g + xlab(input$x) + ylab(input$y)
-        if(input$smooth & input$type == "Scatter Plot") {
-            g <- g + geom_smooth(method = "lm", se = TRUE)
+        if(input$freq_subtype == "Density Plot") {
+            alpha = 0.2
+        } else {
+            alpha = 1
         }
-        if(input$type == "Scatter Plot") {
-            g + geom_point()
-        } else if(input$type == "Box Plot") {
-            g + geom_boxplot()
+        args <- list(alpha = alpha)
+        if(input$freq_color != "None") {
+            data$color <- diamonds[[input$freq_color]]
+            if(input$freq_subtype == "Density Plot") {
+                aes <- aes(x = x, color = color)
+            } else {
+                aes <- aes(x = x, fill = color)
+            }
+        } else {
+            aes <- aes(x = x)
+            if(input$freq_subtype == "Density Plot") {
+                args$color = "steelblue"
+            } else {
+                args$fill = "steelblue"
+            }
         }
+        if(input$freq_facet != "None") {
+            data$facet <- diamonds[[input$freq_facet]]
+            if(input$freq_rotate) {
+                facet <- facet_grid(facet ~ .)
+            } else {
+                facet <- facet_grid(. ~ facet)
+            }
+        } else {
+            facet <- NULL
+        }
+        if(input$freq_subtype == "Histogram") {
+            args$bins <- input$freq_bins
+        }
+        if(input$freq_rotate) {
+            coord_flip <- coord_flip()
+        } else {
+            coord_flip <- NULL
+        }
+        if(input$freq_subtype == "Bar Plot" & !input$freq_rotate) {
+            x_axis_labels_flip <- theme(axis.text.x = element_text(angle = 90, hjust = 1))
+        } else {
+            x_axis_labels_flip <- NULL
+        }
+        geom <- do.call(geoms[input$freq_subtype], args)
+        ggplot(data, aes) + geom + facet + coord_flip + x_axis_labels_flip +
+            xlab(xlab) + labs(fill = input$freq_color, color = input$freq_color) + 
+            theme(legend.position = "bottom")
+    })
+    
+    output$predictors <- renderUI({
+        available_predictors <- setdiff(names(diamonds), input$depvar)
+        checkboxGroupInput("select_predictors", "Select Predictors", available_predictors)
+    })
+    
+    model <- reactive({
+        ds1 <- data.frame(depvar = diamonds[[input$depvar]])
+        if(length(input$select_predictors) > 0) {
+            ds1[input$select_predictors] <- diamonds[input$select_predictors]
+        }
+        lm(depvar ~ ., ds1)
+    })
+    
+    output$adj_r_squared_text <- renderText({
+        as.character(round(summary(model())$adj.r.squared, 5))
+    })
+    
+    output$model_coef_table <- renderTable({
+        summary(model())$coef
+    }, rownames = T)
+    
+    output$diag <- renderPlot({
+        par(mfrow = c(2, 2))
+        plot(model(), which = 1:4)
     })
 })
--- App1/ui.R
+++ App1/ui.R
@@ -1,10 +1,9 @@
 library(shiny)
 library(ggplot2)
-library(plotly)
 data(diamonds)
 
 shinyUI(navbarPage(
-    "Data Explorer",
+    "Diamonds Analysis",
     tabPanel("Data Overview",
              fluidRow(
                  column(3, align = "center",
@@ -14,23 +13,104 @@ shinyUI(navbarPage(
                         hr(),
                         strong(textOutput("var_summary_title")),
                         br(),
-                        tableOutput("var_summary"),
-                        strong(textOutput("plot_var_title")),
-                        plotOutput("plot_var", height = "200px")),
-                 column(9, align = "center",
+                        tableOutput("var_summary")),
+                 column(9, align = "left",
+                        h4("Diamonds Data"),
                         tableOutput("data_head"),
-                        textOutput("data_head_n"))
+                        textOutput("data_head_n"),
+                        br())
              )),
-    tabPanel("Exploratory Analysis",
+    tabPanel("Exploratory Graphs",
              fluidRow(
-                 column(4,
-                        selectInput("type", "Type of Plot", c("Scatter Plot", "Box Plot")),
-                        selectInput("x", "X-Axis Variable", names(diamonds)),
-                        selectInput("y", "Y-Axis Variable", names(diamonds)),
-                        checkboxInput("smooth", "Show Regression Line"),
-                        selectInput("colour", "Colour Variable", c("None", names(diamonds))),
-                        selectInput("facet", "Facet Variable", c("None", names(diamonds)))),
-                 column(8, align = "center",
-                        plotOutput("plot1"))
+                 column(3,
+                        selectInput("type", "Plot Type", c("Scatter Plot", "Box Plot", "Frequency Plot")),
+                        hr(),
+                        conditionalPanel(
+                            condition = "input.type == 'Scatter Plot'",
+                            selectInput("x_scatter", "X Axis Variable", names(diamonds)),
+                            selectInput("y_scatter", "Y Axis Variable", names(diamonds)),
+                            selectInput("color_scatter", "Color Variable", c("None", names(diamonds))),
+                            selectInput("facet_scatter", "Faceting Variable", 
+                                        c("None", names(diamonds)[!sapply(diamonds[1,], is.numeric)])),
+                            sliderInput("alpha_scatter", "Point Opacity", 0.01, 1, 0.5),
+                            numericInput("size_scatter", "Point Size", 1, 1, 10, 1),
+                            selectInput("smooth_scatter", "Smoothing Line", c("None", "Regression Line", "Smooth Curve"))
+                        ),
+                        conditionalPanel(
+                            condition = "input.type == 'Box Plot'",
+                            selectInput("x_box", "X Axis Variable", names(diamonds)),
+                            selectInput("y_box", "Y Axis Variable", 
+                                        names(diamonds)[sapply(diamonds[1,], is.numeric)]),
+                            checkboxInput("rotate_box", "Orientate Plot Horizontally"),
+                            radioButtons("dimension3_box", "Show Another Variable Using:", c("None", "Color", "Faceting")),
+                            conditionalPanel(
+                                condition = "input.dimension3_box == 'Color'",
+                                selectInput("color_box", "Color Variable", 
+                                            names(diamonds)[!sapply(diamonds[1,], is.numeric)])
+                            ),
+                            conditionalPanel(
+                                condition = "input.dimension3_box == 'Faceting'",
+                                selectInput("facet_box", "Faceting Variable",
+                                            names(diamonds)[!sapply(diamonds[1,], is.numeric)])
+                            )
+                        ),
+                        conditionalPanel(
+                            condition = "input.type == 'Frequency Plot'",
+                            radioButtons("freq_subtype", "Plot Subtype", c("Histogram", "Density Plot", "Bar Plot")),
+                            conditionalPanel(
+                                condition = "input.freq_subtype != 'Bar Plot'",
+                                selectInput("freq_x_numeric", "X Axis Variable",
+                                            names(diamonds)[sapply(diamonds[1,], is.numeric)])
+                            ),
+                            conditionalPanel(
+                                condition = "input.freq_subtype == 'Bar Plot'",
+                                selectInput("freq_x_discrete", "X Axis Variable",
+                                            names(diamonds)[!sapply(diamonds[1,], is.numeric)])
+                            ),
+                            selectInput("freq_color", "Color Variable",
+                                        c("None", names(diamonds)[!sapply(diamonds[1,], is.numeric)])),
+                            selectInput("freq_facet", "Faceting Variable",
+                                        c("None", names(diamonds)[!sapply(diamonds[1,], is.numeric)])),
+                            conditionalPanel(
+                                condition = "input.freq_subtype == 'Histogram'",
+                                sliderInput("freq_bins", "Number of Bins", 2, 100, 10)
+                            ),
+                            conditionalPanel(
+                                condition = "input.freq_subtype != 'Density Plot'",
+                                checkboxInput("freq_rotate", "Orientate Plot Horizontally")
+                            )
+                        )),
+                 column(9, align = "center",
+                        conditionalPanel(
+                            condition = "input.type == 'Scatter Plot'",
+                            plotOutput("plot_scatter", height = "500px")
+                        ),
+                        conditionalPanel(
+                            condition = "input.type == 'Box Plot'",
+                            plotOutput("plot_box", height = "500px")
+                        ),
+                        conditionalPanel(
+                            condition = "input.type == 'Frequency Plot'",
+                            plotOutput("plot_freq", height = "500px")
+                        ))
+             )),
+    tabPanel("Linear Regression",
+             fluidRow(
+                 column(3,
+                        selectInput("depvar", "Select Variable to Predict",
+                                    names(diamonds)[sapply(diamonds[1,], is.numeric)]),
+                        uiOutput("predictors")),
+                 column(9,
+                        tabsetPanel(
+                            tabPanel("Model Information",
+                                     br(),
+                                     h4("Adjusted R-Squared"),
+                                     textOutput("adj_r_squared_text"),
+                                     br(),
+                                     h4("Model Coefficients"),
+                                     tableOutput("model_coef_table")),
+                            tabPanel("Diagnostic Plots",
+                                     plotOutput("diag", height = "500px"))
+                        ))
              ))
 ))
